7fdbc2f71b85406f49716a6408e250f9625ae4fa
Patol75 <td75013@hotmail.fr>
2021-11-27 10:25:58 +1100

    Fix 'non_zero_initial_melt'; restrict 'constants.py' to necessary values; include more comments.

--- src/MeltChemistryFunctions.py
+++ src/MeltChemistryFunctions.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-from numpy import (arange, array, diff, empty_like, hstack, interp, linspace,
-                   logical_xor, ones, pad, zeros)
+from numpy import (arange, array, diff, empty_like, interp, linspace,
+                   logical_xor, nonzero, ones, pad, zeros, zeros_like)
 from scipy.constants import g
 from scipy.integrate import LSODA
 
@@ -8,32 +8,37 @@ from ChemistryData import part_coeff, quad_poly_coeff, radii, valency
 from Melt import Katz
 from MeltChemistryCompiled import (al_phase_select_array, mineralogy,
                                    solid_composition)
-from constants import (cs_0, elements, eNd, gnt_out, melt_inputs, rho_mantle,
-                       spl_in, T_mantle)
+from constants import cs_0, elements, eNd, gnt_out, melt_inputs, spl_in
 
 
+# Integrate ode system to obtain concentrations in solid and liquid phases
 def run_integrator(part_arr, mask_ode, ode_dX, X_0, P_0, cs_old,
                    X_spl_in, X_gnt_out):
     # Right-hand side of the ode system
     # Equations 3 and 4 of White et al. - JGR: Solid Earth (1992)
     def ode_rhs(X, cs):
         if mnrl_outputs.get(X) is None:  # X-value not yet encountered
+            # Calculate mineralogy at current X-value
             Fn, pn, Dn = mineralogy(X, X_0, P_0, D[mask_step], ri[mask_step],
                                     val[mask_step], *extra_args)
             D_bulk, P_bulk = ones(cs.size), ones(cs.size)
             D_bulk[mask_step], P_bulk[mask_step] = Dn @ Fn, Dn @ pn
+            # Store mineralogy
             mnrl_outputs[X] = [Fn, pn, Dn, D_bulk, P_bulk]
+        # Retrieve from previously stored values
         D_bulk, P_bulk = mnrl_outputs[X][3], mnrl_outputs[X][4]
         return cs / (1 - X) - cs / (D_bulk - P_bulk * X)
 
     # Jacobian of the right-hand side of the ode system
     def ode_jac(X, cs):
+        # Directly retrieve as the jacobian is executed after the rhs
         D_bulk, P_bulk = mnrl_outputs[X][3], mnrl_outputs[X][4]
         return (1 / (1 - X) - 1 / (D_bulk - P_bulk * X)).reshape((1, -1))
 
     mnrl_outputs = {}
     mask_step = ones(mask_ode.sum(), dtype=bool)
 
+    # Reference solid composition when mineral line-up last changed
     Fn_0_spl, Fn_0_gnt = solid_composition(X_0, P_0, quad_poly_coeff, eNd)
     Fn_0 = al_phase_select_array(X_0, X_spl_in, X_gnt_out, Fn_0_spl, Fn_0_gnt)
 
@@ -59,12 +64,14 @@ def run_integrator(part_arr, mask_ode, ode_dX, X_0, P_0, cs_old,
         #         X_0, Fn_0 = solver.t_old, mnrl_outputs[solver.t_old][0]
         #     else:
         #         X_0, Fn_0 = solver.t, Fn
-        if logical_xor(Fn_0, Fn).any():
-            X_0, Fn_0 = 0.99 * solver.t, Fn
+        if logical_xor(Fn_0, Fn).any():  # Mineral line-up changes
+            X_0, Fn_0 = 0.99 * solver.t, Fn  # Fix to avoid small X - X_0
             P_0 = interp(X_0, part_arr["melt_fraction"], part_arr["pressure"])
 
+        # Consider elements whose concentration has decreased a lot exhausted
         mask_step[solver.y / cs_0[mask_ode] < 1e-6] = False
 
+        # Identify elements about to exhaust as indicated by step size collapse
         if solver.step_size < 1e-10:
             first_deriv_inc = solver._lsoda_solver._integrator.rwork[
                 20 + mask_step.size:20 + mask_step.size * 2]
@@ -75,18 +82,21 @@ def run_integrator(part_arr, mask_ode, ode_dX, X_0, P_0, cs_old,
     key_X = min(mnrl_outputs, key=lambda key_val: abs(key_val - X))
     D_bulk, P_bulk = mnrl_outputs[key_X][3], mnrl_outputs[key_X][4]
 
+    # Store resulting concentrations after successful integration
     cs, cl = zeros(len(elements)), zeros(len(elements))
     cs[mask_ode] = solver.y[mask_step]
     cl[mask_ode] = cs[mask_ode] * (1 - X) / (D_bulk - P_bulk * X)[mask_step]
 
+    # Identify elements with negative concentrations, either in solid or liquid
     mask_neg = (cs <= 0) | (cl < 0)
     mask_ode[mask_neg], cs[mask_neg], cl[mask_neg] = False, 0, 0
 
     return mask_ode, solver.step_size, X_0, P_0, mnrl_outputs[key_X][0], cs, cl
 
 
+# Determine melt fractions associated with spinel-garnet transition
 def calc_X_spl_in_gnt_out(X_spl_in, X_gnt_out, old_depth, current_depth,
-                          part_arr, dTdP_GPa):
+                          part_arr, dTdP_GPa, rho_mantle):
     if current_depth <= spl_in <= old_depth:
         # Interpolate melt fraction at which spinel becomes a stable phase
         X_spl_in = interp(spl_in, part_arr["pressure"][::-1] * 1e9 / rho_mantle
@@ -110,25 +120,38 @@ def calc_X_spl_in_gnt_out(X_spl_in, X_gnt_out, old_depth, current_depth,
     return X_spl_in, X_gnt_out
 
 
-def non_zero_initial_melt(part_arr, pot_temp):
+# Deal with particles initialised over the solidus
+def non_zero_initial_melt(part_arr, part_pot_temp, mant_pot_temp, lab_pressure,
+                          dTdP_GPa):
+    step = 0.02  # Linear pressure path
     P = linspace(6, part_arr["pressure"][0],
-                 int((6 - part_arr["pressure"][0]) / 0.02 + 1))
-    # Linearly distribute cooling of potential temperature over last GPa; 51
-    # pressures for an increment of 0.02 GPa in 1 GPa
-    T = part_arr["temperature"][0] + pad(linspace(T_mantle - pot_temp, 0, 51),
-                                         (P.size - 51, 0),
-                                         constant_values=T_mantle - pot_temp)
+                 int((6 - part_arr["pressure"][0]) / step + 1))
+
+    if part_arr["pressure"][0] > lab_pressure - step:  # Purely adiabatic
+        T = mant_pot_temp + P * dTdP_GPa
+    else:  # Account for decrease in potential temperature
+        distrib_temp_over = int(
+            (lab_pressure - part_arr["pressure"][0]) / step + 1)
+        T = part_pot_temp + P * dTdP_GPa + pad(
+                linspace(mant_pot_temp - part_pot_temp, 0, distrib_temp_over),
+                (P.size - distrib_temp_over, 0),
+                constant_values=mant_pot_temp - part_pot_temp)
+    T[-1] = part_arr["temperature"][0]
+
     X = empty_like(P)
     for i in range(P.size):  # Fictitious, prior melting path
         X[i] = Katz().KatzPT(P[i], T[i], inputConst=melt_inputs)
     assert X[0] == 0 and X[-1] == part_arr["melt_fraction"][0]
 
-    mask = [x > 0 for x in X]  # Identify when melting occurs
-    mask[mask.index(True) - 1] = True  # Include entry before solidus
-    mask[-1] = False  # Remove duplicate
-    part_arr["pressure"] = hstack((P[mask], part_arr["pressure"]))
-    part_arr["temperature"] = hstack((T[mask], part_arr["temperature"]))
-    part_arr["melt_fraction"] = hstack((X[mask], part_arr["melt_fraction"]))
+    mask = zeros_like(X, dtype=bool)
+    max_X = 0
+    for i in range(mask.size):
+        if X[i] > max_X:  # Only keep steps where melting happens
+            max_X = X[i]
+            mask[i] = True
+    mask[nonzero(mask)[0][0] - 1] = True  # Include entry before solidus
+    part_arr["pressure"], part_arr["temperature"] = P[mask], T[mask]
+    part_arr["melt_fraction"] = X[mask]
     assert all(diff(part_arr["melt_fraction"]) > 0)
 
     return part_arr
--- src/constants.py
+++ src/constants.py
@@ -1,48 +1,26 @@
 #!/usr/bin/env python3
 from numpy import array
-from scipy.constants import g
-
 from ChemistryData import DM_SS_2004, PM_MO_1995
 
-domain_dim = (1980e3, 660e3)
-T_surface, T_mantle = 273, 1598
-vel_x = 2.1 / 100 / 365.25 / 8.64e4
-kappa = 1e-6
-rho_mantle = 3300
-alpha = 3e-5
-c_P = 1187
-adiab_grad = alpha * T_mantle * g / c_P
-
-attrib = 'katz_mckenzie_bdd21_'
 # Parameters to provide to the melting functions; only parameters described in
 # Katz et al. (2003) are valid; parameters names must match these defined in
 # __init__ of Katz within Melt.py
-melt_inputs = {"alpha_s": alpha, "B1": 1520 + 273.15, "beta2": 1.2, "c_P": c_P,
+melt_inputs = {"alpha_s": 3e-5, "B1": 1520 + 273.15, "beta2": 1.2, "c_P": 1187,
                "deltaS": 407, "M_cpx": 0.18, "r0": 0.94, "r1": -0.1,
-               "rho_s": rho_mantle, "X_H2O_bulk": 0.01}
+               "rho_s": 3300, "X_H2O_bulk": 0.01}
 # Elements to include within the concentration calculations; each element must
 # have an associated valency, radius and partition coefficient list within
 # ChemistryData.py
 elements = ['La', 'Ce', 'Pr', 'Nd', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er',
             'Tm', 'Yb', 'Lu', 'Na', 'Ti', 'Hf', 'Rb', 'Sr', 'Th', 'U', 'Pb',
             'Nb', 'Zr', 'Y', 'Ta', 'Sc', 'V', 'Cr', 'K', 'P', 'Ba']
+# Value to weigh the contributions of primitive and depleted mantle sources,
+# with 0 a pure primitive end-member and 10 its depleted counterpart
 eNd = 10
 assert 0 <= eNd <= 10
+# Initial solid concentration of included elements
 cs_0 = array([(10 - eNd) / 10 * PM_MO_1995[element]
               + eNd / 10 * DM_SS_2004[element] for element in elements])
+# Depths associated with the spinel-garnet transition
 gnt_out, spl_in = 69e3, 70e3
 assert gnt_out <= spl_in
-
-# Rheology:
-mu_max, mu_min = 1e25, 1e18
-
-# Diffusion creep
-Ediff_UM = 3e5
-Vdiff_UM = 4e-6
-Adiff_UM = 3e-11
-
-# Dislocationn creep
-n = 3.5
-Adisl_UM = 5e-16
-Edisl_UM = 540e3
-Vdisl_UM = 16e-6
